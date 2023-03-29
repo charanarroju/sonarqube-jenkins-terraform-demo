f67f5ae26afbdface2ca1293a93e76f5cdd7f96d
charan <charan@gmail.com>
2023-03-29 23:51:44 +0530

    provider file modified

--- instance.tf
+++ instance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" {
-  ami                         = var.image_id     #"ami-09d56f8956ab235b3"
+  ami                         = var.image_id     #"ami-007855ac798b5175e"
   instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
   count                       = var.inst_count
-  availability_zone           = "us-east-1c"
-  key_name                    = "awsdemokey"
+  availability_zone           = "us-east-1a"
+  key_name                    = "practical"
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
 
   tags = var.ec2_instance_tags
